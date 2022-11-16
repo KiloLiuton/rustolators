c1f1074182a6d39b1ae2a86118d93d3429e4df11
Kevin Liu Rodrigues <kkevin.liu@gmail.com>
2022-11-16 15:49:38 -0300

    Create regular ring

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
-fn main() {
-    const N: usize = 4;
-    const K: usize = 1;
+const N: u32 = 8;
+const K: u32 = 1;
 
-    let mut neighbors: [usize; 2 * N * K] = [0; 2 * N * K];
-    //let mut neighbors: [usize; 2 * N * K] = [
-    //    3, 1,
-    //    0, 2,
-    //    1, 3,
-    //    2, 4,
-    //];
+fn get_right_neighbor(s: u32, n: u32) -> u32 {
+    if (s + n) >= N {
+        return (s + n) - N;
+    }
+    s + n
+}
+
+fn get_left_neighbor(s: u32, n: u32) -> u32 {
+    if s < n {
+        return (s + N) - n;
+    }
+    s - n
+}
+
+fn main() {
+    let mut neighbors: [u32; (2 * N * K) as usize] = [0; (2 * N * K) as usize];
+    let mut indexes: [u32; N as usize] = [0; N as usize];
+    // let mut states: [u8; N as usize] = [0; N as usize];
+
     for i in 0 .. N {
-        for j in 0 .. 2*K {
-            println!("{}{}", i, j);
-            neighbors[i * 2 * K + j] = 666;
+        let n = i * 2 * K;
+        indexes[i as usize] = n;
+        for j in 0 .. K {
+            neighbors[(n + j) as usize] = get_left_neighbor(i, j+1);
+            neighbors[(n + j + 1) as usize] = get_right_neighbor(i, j+1);
         }
     }
     for n in neighbors {
